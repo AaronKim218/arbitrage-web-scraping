56784f12fad4d60e763bf8fbcdeed9a316459e1b
Quinn Vandellos <qvandellos@gmail.com>
2023-12-26 15:03:54 -0500

    added process.exit

--- main.js
+++ main.js
@@ -14,6 +14,7 @@ async function main() {
         // console.log('teams', teams);
         
         await findArbitrage(matchups, teams, logger);
+        process.exit(0);
     }
     catch(e)
     {
