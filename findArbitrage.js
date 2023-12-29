e9411ab61820aee92a0686618996e448cc023675
aaronkimbrooktec <218adk@gmail.com>
2023-12-28 19:03:14 -0500

    add pinnacle scraper

--- findArbitrage.js
+++ findArbitrage.js
@@ -7,6 +7,7 @@ const fanduelNBA = 'https://sportsbook.fanduel.com/basketball?tab=nba';
 const draftkingsNBA = 'https://sportsbook.draftkings.com/leagues/basketball/nba';
 const espnbetNBA = 'https://espnbet.com/sport/basketball/organization/united-states/competition/nba/featured-page'
 const betriversNBA = 'https://nj.betrivers.com/?page=sportsbook&group=1000093652&type=matches'
+const pinnacleNBA = 'https://www.pinnacle.com/en/basketball/matchups/'
 
 
 export default async function findArbitrage(games, teams, logger){
@@ -15,6 +16,7 @@ export default async function findArbitrage(games, teams, logger){
     await draftkingsScraper(teams);
     await espnbetScraper(teams);
     await betriversScraper(teams);
+    await pinnacleScraper(teams);
 
     // console.log(oddsArray);
 
@@ -272,6 +274,81 @@ async function betriversScraper(teams) {
             console.log(teams[i], 'betrivers')
             console.log('prob', probability)
             oddsArray[3][TEAMINDICES.get(teams[i])] = probability;
+            
+        }
+        catch(e) {
+            console.log(e.stack);
+            console.log(e.name);
+            console.log(e.message);
+
+        }
+    }
+
+    console.log('odds arr', oddsArray)
+    console.log('betriversScraper() done');
+
+}
+
+async function pinnacleScraper(teams) {
+
+    puppeteer.use(StealthPlugin());
+
+    const browser = await puppeteer.launch({ headless: "new" })
+    const page = await browser.newPage()
+    await page.goto(pinnacleNBA, { waitUntil: 'networkidle2' })
+    for(let i = 0; i < teams.length; i++)
+    {
+        try{
+
+            const elementsHTML = await page.$$eval(`span`, (spans, teamName) => 
+                spans.map(span => {
+                    if (span.innerText.includes(teamName)) {
+                        // Check the position of the span's parent within its parent
+                        let parentElement = span.parentElement;
+                        let childIndex = null;
+                        if (parentElement && parentElement.parentElement) {
+                            let children = Array.from(parentElement.parentElement.children);
+                            childIndex = children.indexOf(parentElement);
+                        }
+
+                        if (childIndex === null) return null; // Parent's position is not determined
+
+                        // Navigate to the 7th parent
+                        let seventhParent = span;
+                        for (let i = 0; i < 7; i++) {
+                            if (seventhParent.parentElement) {
+                                seventhParent = seventhParent.parentElement;
+                            } else {
+                                // Not enough parents, return null
+                                return null;
+                            }
+                        }
+
+                        // Get the 4th child of the 7th parent
+                        let fourthChildOfSeventhParent = seventhParent.children[3];
+                        if (!fourthChildOfSeventhParent) return null;
+
+                        // Depending on the initial position (first or second child), select the corresponding child
+                        let targetChild = (childIndex === 0) ? fourthChildOfSeventhParent.children[0] : fourthChildOfSeventhParent.children[1];
+                        if (!targetChild) return null;
+
+                        // Find the span with the specified class and return its text
+                        let priceSpan = targetChild.querySelector('.style_price__3Haa9');
+                        return priceSpan ? priceSpan.innerText : null;
+                    }
+                    return null;
+                }).filter(item => item !== null),
+                teams[i]
+            );
+            
+            let text = elementsHTML[0];
+
+            console.log('span text', text);
+            
+            let probability = parseFloat(calculateProbability(parseFloat(text)));
+            console.log(teams[i], 'pinnacle')
+            console.log('prob', probability)
+            oddsArray[4][TEAMINDICES.get(teams[i])] = probability;
             
         }
         catch(e) {
